0cf947ec008ee8a6c28d3628bcc06445dda50ba2
alexivxii <alex.munteanu58@gmail.com>
2023-07-22 21:49:17 +0300

    tried implementing toolbar on details page with back button, encountered error

--- app/src/main/java/com/example/testfetchdata/PostDetailsActivity.kt
+++ app/src/main/java/com/example/testfetchdata/PostDetailsActivity.kt
@@ -2,13 +2,25 @@ package com.example.testfetchdata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+import androidx.core.app.NavUtils
+
 
 class PostDetailsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post_details)
+
+        //setting the toolbar
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        //getting the arguments sent to this page and binding them to the textViews
 
         val titleTextView = findViewById<TextView>(R.id.titleTextView)
         val bodyTextView = findViewById<TextView>(R.id.bodyTextView)
@@ -20,5 +32,21 @@ class PostDetailsActivity : AppCompatActivity() {
         titleTextView.text = postTitle
         bodyTextView.text = postBody
     }
+
+    //handle the back button click
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            android.R.id.home -> {
+                // Handle the Up button press by navigating up to the parent activity (MainActivity)
+                NavUtils.navigateUpFromSameTask(this)
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
 
